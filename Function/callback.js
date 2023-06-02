605e329fca7467ab7c0c72270d60c2b56323f7d0
mdimtias <developertanbir@gmail.com>
2023-06-02 08:06:27 +0600

    Callback example two

--- Function/callback.js
+++ Function/callback.js
@@ -8,3 +8,37 @@ function displayResult(value) {
 }
 
 calculate(5, 3, displayResult);
+
+// Example Two 
+function mainFunc(a, b, callbackFunc){
+	const c = a+b;
+	const d = a-b;
+	const result = callbackFunc(c, d)
+	return result;
+}
+
+const sum = mainFunc(5, 4, function(c, d){
+	const result = c+d;
+	return result;
+})
+console.log("Sum ", sum);
+
+const substract = mainFunc(5, 4, function(c, d){
+	const result = c-d;
+	return result;
+})
+console.log("Substract", substract);
+
+const multiply = mainFunc(5, 4, function(c, d){
+	const result = c*d;
+	return result;
+})
+console.log("multiply ", multiply);
+
+const devide = mainFunc(5, 4, function(c, d){
+	const result = c/d;
+	return result;
+})
+console.log("devide ", devide);
+
+
