140c33314f5e666b7cc4af9de329b5f64b2cc336
mdimtias <developertanbir@gmail.com>
2023-07-08 09:49:21 +0600

    js array method

--- Array/arrayAllMethods.js
+++ Array/arrayAllMethods.js
@@ -8,13 +8,37 @@ console.log(mapped);
 const filterd = arr.filter(element => element % 2 === 0);
 console.log(filterd);
 
-const fruits = ["Banana", "Orange", "Apple", "Mango", "Strawberry", "Pineapple", "Grape", "Watermelon"];
+const fruits = ["Banana", "Orange", "Apple", "Mango", "Strawberry", "Pineapple", "Grape"];
 // The at() method returns an indexed element from an array.
 const fruit = fruits.at(2); // Default is 0 and -1 returns the last element. Result Apple
 console.log(fruit);
 
 const arr2 = ["A", "B", "C",];
 const arr3 = ["D", "E", "F",];
+// The concat() method concatenates (joins) two or more arrays.
 console.log(arr2.concat(arr3));
 
+// addNewElementLast
+fruits.push("Watermelon");
+// addNewElementFirst
+fruits.unshift("Olive");
+// DeleteLastElement 
+fruits.pop();
+// DeleteFirstElement
+fruits.shift();
+console.log(fruits);
+// fruits.valueOf() returns the same as fruits:
+console.log(fruits.valueOf());
 
+const myFruits = ["Banana", "Orange", "Apple", "Mango"];
+myFruits.reverse();
+console.log(myFruits);
+
+const numbers = [45, 12, 15, 16, 20, 16, 35];
+let total = 0;
+numbers.reduce((initialValue, value, index)=>{
+	total += value;
+}, 0);
+console.log(total);
+console.log(numbers.length); // Total array length
+console.log(numbers.lastIndexOf(16));
