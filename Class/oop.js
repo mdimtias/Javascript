dc3fd111a0da472cd434d5189dd0bd7859706dd5
mdimtias <developertanbir@gmail.com>
2023-08-01 23:07:10 +0600

    Inheritance oop

--- Class/oop.js
+++ Class/oop.js
@@ -14,3 +14,16 @@ const karim = new Person("Karim", "karim@gmail.com");
 console.log(karim.print()); 
 
 console.log(Person.country); // access Static proferty to direct class name
+
+//  Inheritance
+class ManOne extends Person {
+	constructor(name, email, phone){
+		super(name, email);
+		this.phone = phone;
+	}
+	print(){
+		console.log(`My Name is ${this.name} , Email ${this.email} and Phone ${this.phone}`);
+	}
+}
+const personOne = new ManOne("rohim", "rohim@gmail.com", 018120000005);
+console.log(personOne.print());
